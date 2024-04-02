a808a4b4fde38c499cdf0ec9f9ddab93a5e0c9ac
d1or2004 <161047377+d1or2004@users.noreply.github.com>
2024-04-03 00:11:10 +0500

    models

--- library/admin.py
+++ library/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Bookings, Comment
 
 admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Bookings)
+admin.site.register(Comment)
--- student/migrations/0001_initial.py
+++ student/migrations/0001_initial.py
@@ -13,21 +13,18 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Author',
+            name='Address',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('birth_date', models.DateField(auto_now_add=True)),
+                ('name', models.CharField(max_length=40)),
             ],
         ),
         migrations.CreateModel(
-            name='Book',
+            name='Student',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('description', models.TextField()),
-                ('price', models.FloatField()),
-                ('count', models.IntegerField()),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='library.author')),
+                ('age', models.PositiveIntegerField()),
+                ('address', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='student.address')),
             ],
         ),
     ]
